ba9d52221cc75725e2e1dfb2d0380505fd78dfac
Tittaya Mairittha <callmefons@gmail.com>
2016-02-29 21:02:08 +0700

    Refactor to use Angular factory

--- src/todos/todos.js
+++ src/todos/todos.js
@@ -1,6 +1,6 @@
 import _ from 'lodash';
 
-export default function($scope){
+export default function($scope, todoFactory){
 
 	let params = {
 		createHasInput : false
@@ -23,38 +23,52 @@ export default function($scope){
 		todo.isCompleted = !todo.isCompleted;
 	};
 
+	$scope.onCancelClick = todo => {
+		todo.isEditing = false;
+	};
+
 	$scope.onEditClick = todo => {
 		todo.isEditing = true;
 		todo.updateTask = todo.task;
 	};
 
-	$scope.createTask = () => {
-		params.createHasInput = false;
-		$scope.createTaskInput = '';
-	};
+	const {
+		createTask, 
+		updateTask, 
+		deleteTask, 
+		watchCreateTaskInput
+	} = todoFactory;
 
-	$scope.onCancelClick = todo => {
-		todo.isEditing = false;
-	};
+	$scope.createTask = _.partial(createTask, $scope, params);
+	$scope.deleteTask = _.partial(deleteTask, $scope);
+	$scope.updateTask = _.partial(updateTask);
+	$scope.$watch('createTaskInput', _.partial(watchCreateTaskInput, $scope, params));
 
-	$scope.deleteTask = todoToDelete => {
-		_.remove($scope.todos, todo => todo.task === todoToDelete.task);
-	};
 
-	$scope.updateTask = todo => {
-		todo.task = todo.updateTask;
-		todo.isEditing = false;
-	};
+	// $scope.createTask = () => {
+	// 	params.createHasInput = false;
+	// 	$scope.createTaskInput = '';
+	// };
+	
 
-	$scope.$watch('createTaskInput', val => {
-		if(!val && params.createHasInput){
-			$scope.todos.pop();
-			params.createHasInput = false;
-		} else if(val && !params.createHasInput){
-			$scope.todos.push({task: val, isComplete : false});
-			params.createHasInput = true;
-		} else if(val && params.createHasInput){
-			$scope.todos[$scope.todos.length - 1].task = val;
-		}
-	});
+	// $scope.deleteTask = todoToDelete => {
+	// 	_.remove($scope.todos, todo => todo.task === todoToDelete.task);
+	// };
+
+	// $scope.updateTask = todo => {
+	// 	todo.task = todo.updateTask;
+	// 	todo.isEditing = false;
+	// };
+
+	// $scope.$watch('createTaskInput', val => {
+	// 	if(!val && params.createHasInput){
+	// 		$scope.todos.pop();
+	// 		params.createHasInput = false;
+	// 	} else if(val && !params.createHasInput){
+	// 		$scope.todos.push({task: val, isCompleted : false});
+	// 		params.createHasInput = true;
+	// 	} else if(val && params.createHasInput){
+	// 		$scope.todos[$scope.todos.length - 1].task = val;
+	// 	}
+	// });
 }
